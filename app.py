57b29cdb768490aad9c4f26843a8e73fc83ebdf5
Moksh Goyal <221651574+MokshonWork@users.noreply.github.com>
2026-07-10 13:20:31 +0530

    Add upload interface and navigation

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="FutureSight AI",
+    page_icon="🗂️",#online yeh hi smj aaya logo baad mei dekhten hh filal yeh chindi
+    layout="wide"
+)
+
+st.title("🗂️ FutureSight AI")
+st.write("Welcome to FutureSight AI")
+
+st.success("Use the sidebar to open the Upload page.")
